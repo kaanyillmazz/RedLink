dc1e25f338aedb0041b8aa7460e3b22e3f20a39f
Kaan Yılmaz <kaan.yilmaz.21588@ozu.edu.tr>
2022-07-12 01:01:09 +0300

    successfully added placeholder until response comes from the server

--- src/LinkList.js
+++ src/LinkList.js
@@ -17,70 +17,98 @@ const client = axios.create({
     baseURL: "https://mockend.com/kaanyillmazz/redlink/posts"
 });
 
-let postsHolder = [];
+let obj = function(id, title, likes){
+    this.id = id;
+    this.title = title;
+    this.likes = likes;
+}
+let obj1 = new obj(1,"yes", "10");
+let obj2 = new obj(2,"yes", "10");
+let obj3 = new obj(3,"yes", "10");
 
+
+
+
+let postsHolder = [obj1,obj2,obj3];
 
 function LinkList() {
+
     const [open, setOpen] = React.useState(false);
     const [posts, setPosts] = React.useState([]);
+    const [post, setPost] = React.useState([]);
+
 
     React.useEffect(() => {
-        client.get("").then((response) => {
-            setPosts(response.data.splice(1, 3));
-        }).catch(error => {
-            console.log(error)
-        });
+        async function getPosts() {
+            const response = await client.get("");
+            postsHolder = response.data;
+            setPosts(response.data);
+        }
+        getPosts();
     }, []);
+
 
     console.log(posts);
     console.log(posts.at(0));
-    postsHolder = posts;
 
-    const handleFocusEnter = () => {
-        setOpen(true);
-    };
-    const handleFocusLeave = () => {
-        setOpen(false);
-    };
+    window.localStorage.setItem('posts', JSON.stringify(posts));
 
-    let listItem;
-    if (posts) {
-        listItem = posts.map(post => {
-            return (
-                <ListItem alignItems="flex-start" onMouseEnter={handleFocusEnter} onMouseLeave={handleFocusLeave}>
-                    <div>
-                        <Fab size="small"
-                             style={{position: 'absolute', right: 0, bottom: 5, height: 20, width: 20, minHeight: 20}}>
-                            <KeyboardArrowUpIcon/>
-                        </Fab>
-                        <Fab size="small"
-                             style={{position: 'absolute', right: 26, bottom: 5, height: 20, width: 20, minHeight: 20}}>
-                            <KeyboardArrowDownIcon/>
-                        </Fab>
-                    </div>
-                    <Paper variant="outlined" square style={{padding: "1px", marginRight: "10px"}}>
-                        <Grid container textAlign="center" justifyContent="center"
-                              style={{minHeight: 80, maxHeight: 80, minWidth: 80}}>
-                            <Grid item>
-                                <h1 style={{fontSize: "medium"}}>X</h1>
-                                <h1 style={{fontSize: "medium"}}>Points</h1>
-                            </Grid>
+    const MyListItem = ({index}) => {
+        const handleFocusEnter = () => {
+            setOpen(true);
+        };
+        const handleFocusLeave = () => {
+            setOpen(false);
+        };
+
+        const myObject = postsHolder.at(index);
+        const [open, setOpen] = React.useState(false);
+        const [post, setPost] = React.useState(myObject);
+
+
+
+        let ListItem0;
+        if (post) {
+            let str = post.title;
+            let str1 = "https://"+str+".com"
+            ListItem0 = <ListItem alignItems="flex-start" onMouseEnter={handleFocusEnter} onMouseLeave={handleFocusLeave}>
+                <div>
+                    <Fab size="small"
+                         style={{position: 'absolute', right: 0, bottom: 5, height: 20, width: 20, minHeight: 20}}>
+                        <KeyboardArrowUpIcon/>
+                    </Fab>
+                    <Fab size="small"
+                         style={{position: 'absolute', right: 26, bottom: 5, height: 20, width: 20, minHeight: 20}}>
+                        <KeyboardArrowDownIcon/>
+                    </Fab>
+                </div>
+
+                <Paper variant="outlined" square style={{padding: "1px", marginRight: "10px"}}>
+                    <Grid container textAlign="center" justifyContent="center"
+                          style={{minHeight: 80, maxHeight: 80, minWidth: 80}}>
+                        <Grid item>
+                            <h1 style={{fontSize: "medium"}}>{post.likes}</h1>
+                            <h1 style={{fontSize: "medium"}}>Points</h1>
                         </Grid>
-                    </Paper>
-                    <ListItemText
-                        primary={post.title}
-                        secondary={
-                            <React.Fragment>
-                                {"I'll be in your neighborhood doing errands this"}
-                            </React.Fragment>
-                        }/>
+                    </Grid>
+                </Paper>
 
-                    <AlertDialogNew show={open}/>
+                <ListItemText
+                    primary={str}
+                    secondary={
+                        <React.Fragment>
+                            {str1}
+                        </React.Fragment>
+                    }/>
 
-                </ListItem>
-            )
-        });
+                <AlertDialogNew show={open}/>
+
+            </ListItem>
+        }
+        return ListItem0;
     }
+
+
 
     return (
         <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
@@ -90,62 +118,10 @@ function LinkList() {
             <MyListItem index={1}/>
             <Divider variant="inset" component="li"/>
             <MyListItem index={2}/>
-
         </List>
     );
 }
 
-const MyListItem = ({index}) => {
-    const handleFocusEnter = () => {
-        setOpen(true);
-    };
-    const handleFocusLeave = () => {
-        setOpen(false);
-    };
 
-    const [open, setOpen] = React.useState(false);
-
-    const post0 = postsHolder.at(index);
-
-    let ListItem0;
-    if (post0) {
-        let str = post0.title;
-        let str1 = "https://"+str+".com"
-        ListItem0 = <ListItem alignItems="flex-start" onMouseEnter={handleFocusEnter} onMouseLeave={handleFocusLeave}>
-            <div>
-                <Fab size="small"
-                     style={{position: 'absolute', right: 0, bottom: 5, height: 20, width: 20, minHeight: 20}}>
-                    <KeyboardArrowUpIcon/>
-                </Fab>
-                <Fab size="small"
-                     style={{position: 'absolute', right: 26, bottom: 5, height: 20, width: 20, minHeight: 20}}>
-                    <KeyboardArrowDownIcon/>
-                </Fab>
-            </div>
-
-            <Paper variant="outlined" square style={{padding: "1px", marginRight: "10px"}}>
-                <Grid container textAlign="center" justifyContent="center"
-                      style={{minHeight: 80, maxHeight: 80, minWidth: 80}}>
-                    <Grid item>
-                        <h1 style={{fontSize: "medium"}}>{post0.likes}</h1>
-                        <h1 style={{fontSize: "medium"}}>Points</h1>
-                    </Grid>
-                </Grid>
-            </Paper>
-
-            <ListItemText
-                primary={str}
-                secondary={
-                    <React.Fragment>
-                        {str1}
-                    </React.Fragment>
-                }/>
-
-            <AlertDialogNew show={open}/>
-
-        </ListItem>
-    }
-    return ListItem0;
-}
 
 export default LinkList;
