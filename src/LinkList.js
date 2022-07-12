d57e37f4441e00e9d631a19ec623cff9f3451378
Kaan Yılmaz <kaan.yilmaz.21588@ozu.edu.tr>
2022-07-12 17:15:16 +0300

    implemented delete

--- src/LinkList.js
+++ src/LinkList.js
@@ -8,25 +8,35 @@ import ListItemText from '@mui/material/ListItemText';
 import {Box, Grid, Paper} from '@mui/material'
 import Fab from '@mui/material/Fab';
 
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import EditIcon from '@mui/icons-material/Edit';
+
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-import AlertDialogNew from "./RemovePopup";
+
+import Button from "@mui/material/Button";
 
 const client = axios.create({
     baseURL: "https://mockend.com/kaanyillmazz/redlink/posts"
 });
 
-let obj = function(id, title, points){
+let obj = function (id, title, points) {
     this.id = id;
     this.title = title;
     this.points = points;
 }
-let obj1 = new obj(1,"Loading...", "0");
-let obj2 = new obj(2,"Loading...", "0");
-let obj3 = new obj(3,"Loading...", "0");
+let obj1 = new obj(1, "Loading...", "0");
+let obj2 = new obj(2, "Loading...", "0");
+let obj3 = new obj(3, "Loading...", "0");
 
-let postsHolder = [obj1,obj2,obj3];
+let postsHolder = [obj1, obj2, obj3];
 
 function LinkList() {
 
@@ -41,6 +51,7 @@ function LinkList() {
             postsHolder = response.data;
             setPosts(response.data);
         }
+
         getPosts();
     }, []);
 
@@ -51,32 +62,71 @@ function LinkList() {
     window.localStorage.setItem('posts', JSON.stringify(posts));
 
     const MyListItem = ({index}) => {
+        const AlertDialogNew = ({show}) => {
+            const [open, setOpen] = React.useState(false);
+            const handleClick = () => {
+                setOpen(true);
+            };
+            const handleClose = () => {
+                setOpen(false);
+            };
+            const handleDelete = () => {
+                deleteHandler();
+                setOpen(false);
+            };
+
+            let box;
+            if (show) {
+                box = <Box sx={{'& > :not(style)': {m: 1}}}>
+                    <Fab size="small" color="secondary" aria-label="edit" style={{position: 'absolute'}}>
+                        <DeleteOutlineIcon onClick={handleClick}/>
+                    </Fab>
+                </Box>
+            }
+            return (
+                <div>
+                    {box}
+                    <Dialog
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description">
+                        <DialogTitle id="alert-dialog-title">
+                            {"Are you sure you want to delete this?"}
+                        </DialogTitle>
+                        <DialogActions>
+                            <Button onClick={handleClose}>No</Button>
+                            <Button onClick={handleDelete} autoFocus> Yes </Button>
+                        </DialogActions>
+                    </Dialog>
+                </div>
+            );
+        }
+
         const handleFocusEnter = () => {
             setOpen(true);
         };
         const handleFocusLeave = () => {
             setOpen(false);
         };
-
         const myObject = postsHolder.at(index);
         const [open, setOpen] = React.useState(false);
         const [post, setPost] = React.useState(myObject);
 
         const likeHandler = async () => {
             const id = post.id;
-            const article = {id: id, points: post.points+1, title: post.title};
+            const article = {id: id, points: post.points + 1, title: post.title};
             const response = await client.put(`/${id}`, article);
-            response.data = {id: id, points: post.points+1, title: post.title}
+            response.data = {id: id, points: post.points + 1, title: post.title}
             setPost(response.data);
             console.log(response.data);
-
         };
 
         const dislikeHandler = async () => {
             const id = post.id;
-            const article = {id: id, points: post.points-1, title: post.title};
+            const article = {id: id, points: post.points - 1, title: post.title};
             const response = await client.put(`/${id}`, article);
-            response.data = {id: id, points: post.points-1, title: post.title}
+            response.data = {id: id, points: post.points - 1, title: post.title}
             setPost(response.data);
             console.log(response.data);
         };
@@ -91,46 +141,43 @@ function LinkList() {
         let ListItem0;
         if (post) {
             let str = post.title;
-            let str1 = "https://"+str+".com"
-            ListItem0 = <ListItem alignItems="flex-start" onMouseEnter={handleFocusEnter} onMouseLeave={handleFocusLeave}>
-                <div>
-                    <Fab onClick={function() { likeHandler(); }}
-                        size="small"
-                         style={{position: 'absolute', right: 0, bottom: 5, height: 20, width: 20, minHeight: 20}}>
-                        <KeyboardArrowUpIcon/>
-                    </Fab>
-                    <Fab onClick={function() { dislikeHandler(); }}
-                        size="small"
-                         style={{position: 'absolute', right: 26, bottom: 5, height: 20, width: 20, minHeight: 20}}>
-                        <KeyboardArrowDownIcon/>
-                    </Fab>
-                </div>
-
-                <Paper variant="outlined" square style={{padding: "1px", marginRight: "10px"}}>
-                    <Grid container textAlign="center" justifyContent="center"
-                          style={{minHeight: 80, maxHeight: 80, minWidth: 80}}>
-                        <Grid item>
-                            <h1 style={{fontSize: "medium"}}>{post.points}</h1>
-                            <h1 style={{fontSize: "medium"}}>Points</h1>
+            let str1 = "https://" + str + ".com"
+            ListItem0 =
+                <ListItem alignItems="flex-start" onMouseEnter={handleFocusEnter} onMouseLeave={handleFocusLeave}>
+                    <div>
+                        <Fab onClick={function () {likeHandler();}}
+                             size="small"
+                             style={{position: 'absolute', right: 0, bottom: 5, height: 20, width: 20, minHeight: 20}}>
+                            <KeyboardArrowUpIcon/>
+                        </Fab>
+                        <Fab onClick={function () {dislikeHandler();}}
+                             size="small"
+                             style={{position: 'absolute', right: 26, bottom: 5, height: 20, width: 20, minHeight: 20}}>
+                            <KeyboardArrowDownIcon/>
+                        </Fab>
+                    </div>
+                    <Paper variant="outlined" square style={{padding: "1px", marginRight: "10px"}}>
+                        <Grid container textAlign="center" justifyContent="center"
+                              style={{minHeight: 80, maxHeight: 80, minWidth: 80}}>
+                            <Grid item>
+                                <h1 style={{fontSize: "medium"}}>{post.points}</h1>
+                                <h1 style={{fontSize: "medium"}}>Points</h1>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </Paper>
-
-                <ListItemText
-                    primary={str}
-                    secondary={
-                        <React.Fragment>
-                            {str1}
-                        </React.Fragment>
-                    }/>
-
-                <AlertDialogNew show={open}/>
-
-            </ListItem>
+                    </Paper>
+                    <ListItemText
+                        primary={str}
+                        secondary={
+                            <React.Fragment>
+                                {str1}
+                            </React.Fragment>
+                        }/>
+                    <AlertDialogNew show={open}/>
+                </ListItem>
         }
+
         return ListItem0;
     }
-
 
     return (
         <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
@@ -143,7 +190,4 @@ function LinkList() {
         </List>
     );
 }
-
-
-
 export default LinkList;
